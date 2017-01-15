6438f9d6b48113c7eff4a3478a8f83c5d9b7716c
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-15 20:34:33 -0200

    Add inputs for saving a course on course-page component

--- src/components/course/courses-page.js
+++ src/components/course/courses-page.js
@@ -1,10 +1,41 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            course: {title: ''},
+        };
+
+        this.OnTitleChange = this.OnTitleChange.bind(this);
+        this.OnClickSave = this.OnClickSave.bind(this);
+    }
+
+
+    OnTitleChange(event) {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course: course});
+    }
+
+    OnClickSave(event) {
+        alert(`Saving ${this.state.course.title}`);
+    }
+
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                <h2>Add Courses</h2>
+                <input
+                    type="text"
+                    onChange={this.OnTitleChange}
+                    value={this.state.course.title}/>
+                <input
+                    type="submit"
+                    onClick={this.OnClickSave}
+                    value="save"/>
             </div>
         );
     }
