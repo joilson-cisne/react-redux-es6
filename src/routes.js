057e2297aa1144d2ec8e066fa43c14f96fc5ede4
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-15 02:41:06 -0200

    Add courses page

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/app';
 import HomePage from './components/home/home-page';
 import AboutPage from './components/about/about-page';
+import CoursePage from './components/course/courses-page';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
+        <Route path="courses" component={CoursePage}/>
         <Route path="about" component={AboutPage}/>
     </Route>
 );
