e7777591f2f1398f943ce8ca998a5315ebc6ba0b
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-15 20:30:13 -0200

    Remove unused props

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,7 +1,7 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import {Link, IndexLink} from 'react-router';
 
-const Header = ({loading}) => {
+const Header = () => {
     return (
         <nav>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -11,10 +11,6 @@ const Header = ({loading}) => {
             <Link to="/about" activeClassName="active">About</Link>
         </nav>
     );
-};
-
-Header.propTypes = {
-    loading: PropTypes.bool.isRequired,
 };
 
 export default Header;
