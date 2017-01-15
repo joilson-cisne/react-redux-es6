563803ff26fb490215cf10aeba8fad61ea56f9df
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-15 20:32:58 -0200

    Add course reducer

---
+++ src/reducers/course-reducer.js
@@ -0,0 +1,12 @@
+export default (state = [], action) => {
+    switch (action.type) {
+    case 'CREATE_COURSE':
+        return [
+            ...state,
+            Object.assign({}, state, action.course),
+        ];
+
+    default:
+        return state;
+    }
+};
