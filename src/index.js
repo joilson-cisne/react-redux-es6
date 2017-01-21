50535a0e97ed4cd4e5097549469995e00ee3ac70
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-20 23:56:50 -0200

    Dispatch action on page load

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import {Router, browserHistory} from 'react-router';
 import {Provider} from 'react-redux';
 
 import {configureStore} from './store/configure-store';
+import {loadCourses} from './actions/course-actions';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
+
 render(
     <Provider store={store}>
         <Router history={browserHistory} routes={routes}/>
