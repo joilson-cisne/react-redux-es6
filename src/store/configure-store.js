dc9206a6782b4c3bd13aee4b4f5629dfaced7d4f
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-20 23:18:19 -0200

    Import redux-thunk

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,12 +1,14 @@
 import {createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers/index';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
+
+import rootReducer from '../reducers/index';
 
 const configureStore = (initialState) => {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(reduxImmutableStateInvariant())
+        applyMiddleware(thunk, reduxImmutableStateInvariant())
     );
 };
 
