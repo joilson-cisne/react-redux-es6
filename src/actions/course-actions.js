bc8cc28a77a42d20f7d893f5a5460a1f4f347fcc
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-20 23:36:59 -0200

    Create load courses thunk

--- src/actions/course-actions.js
+++ src/actions/course-actions.js
@@ -1,7 +1,20 @@
 import * as types from './action-types';
+import CourseApi from '../api/mock-course-api';
 
-const createCourse = (course) => {
-    return {type: types.CREATE_COURSE, course};
+const loadCoursesSuccess = (courses) => {
+    return {type: types.LOAD_COURSES_SUCCESS, courses};
 };
 
-export {createCourse};
+const loadCourses = () => {
+    return (dispatch) => {
+        return CourseApi.getAllCourses()
+            .then( courses => {
+                dispatch(loadCoursesSuccess(courses));
+            })
+            .catch(error => {
+                throw(error);
+            });
+    };
+}
+
+export {loadCourses};
