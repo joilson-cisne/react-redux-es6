83c6d6f265d40604dc8fd7c99544e915947c515c
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-26 11:32:32 -0200

    Add redux container component -  ManageCoursePage

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,14 @@ import App from './components/app';
 import HomePage from './components/home/home-page';
 import AboutPage from './components/about/about-page';
 import CoursePage from './components/course/courses-page';
+import ManageCoursePage from './components/course/manage-course-page';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
         <Route path="courses" component={CoursePage}/>
+        <Route path="course" component={ManageCoursePage}/>
+        <Route path="course/:id" component={ManageCoursePage}/>
         <Route path="about" component={AboutPage}/>
     </Route>
 );
