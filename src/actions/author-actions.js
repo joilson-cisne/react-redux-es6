5cd273199d9e718d9ebf7de46709243b5558f523
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-27 20:52:35 -0200

    Create author actions

---
+++ src/actions/author-actions.js
@@ -0,0 +1,18 @@
+import AuthorApi from '../api/mock-author-api';
+import * as types from './action-types';
+
+export function loadAuthorsSuccess(authors) {
+    return {type: types.LOAD_AUTHORS_SUCCESS, authors};
+}
+
+export function loadAuthors() {
+    return (dispatch) => {
+        return AuthorApi.getAllAuthors()
+            .then(authors => {
+                dispatch(loadAuthorsSuccess(authors));
+            })
+            .catch(error => {
+                throw(error);
+            });
+    };
+}
