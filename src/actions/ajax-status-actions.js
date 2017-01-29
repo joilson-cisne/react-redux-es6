38ba02a8cb10996c784299fcf127ab39b77de236
Joilson Cisne <joilsoncisne@gmail.com>
2017-01-29 15:08:39 -0200

    Create ajax status actions

---
+++ src/actions/ajax-status-actions.js
@@ -0,0 +1,6 @@
+import * as types from './action-types';
+
+export function beginAjaxCall() {
+    return {type: types.BEGIN_AJAX_CALL};
+}
+
--- src/reducers/initial-state.js
+++ src/reducers/initial-state.js
@@ -1,4 +1,5 @@
 export default {
     courses: [],
     authors: [],
+    numAjaxCallsInProgress: 0,
 };
